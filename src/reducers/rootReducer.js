b1b55fd2f65baaa6e0fd7ab67bff9513695a8af2
Phalgun Mittal <phalgun99@gmail.com>
2020-07-19 21:34:51 +0530

    Update rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -8,31 +8,44 @@ const rootReducer = (state = initialState, action) => {
     switch(action.type)
     {
             case actionTypes.POST_USERS:
-              console.log(action.payload.entry)
             return {
                 ...state,
                 notes:[...state.notes, action.payload.entry]
             }
 
         case actionTypes.DELETE_USERS:
-              console.log(action.payload.id)
               const updatedArray = state.notes.filter(notes => notes.id !== action.payload.id);
             return {
                 ...state,
                 notes: updatedArray
             }
 
+
+        case actionTypes.EDIT_USERS:
+            const updatedUsers = state.notes.map(note => {
+            if (note.id === action.payload.id) {
+                return action.payload;
+            }
+            else
+            {
+                return note;
+            }});
+            return {
+                ...state,
+                notes: updatedUsers
+            };
+
+
         case actionTypes.RECEIVE_USERS:
-               console.log(action.payload.data)
             return {
                 ...state,
                 notes: action.payload.data   
             }
 
+
         case actionTypes.FETCH_FAILURE:
             return {
               ...state,
-              loading: false,
               error: action.payload.error,
               notes: []
             }
